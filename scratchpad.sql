876085853efc3611c9729ce6862b521d0948e05d
Ndil-Adjim Sou Ngagoy <nasoungadoy@gmail.com>
2018-06-13 23:46:06 +0100

    change queries to uppercase for reserved keywords

--- scratchpad.sql
+++ scratchpad.sql
@@ -1,32 +1,30 @@
 -- 1. What are the most popular three articles of all time?
-select articles.title, count(*) from articles, log where path like '%' || slug || '%' group by articles.title;
+SELECT articles.title, COUNT(articles.title)
+  FROM articles, log
+  WHERE path LIKE '%' || slug || '%'
+  GROUP BY articles.title;
 
-select articles.title, count(*) as hits
-  from articles, log
-  where path like '%' || slug || '%'
-  group by articles.title
-  order by hits desc
+SELECT articles.title, COUNT(articles.title) AS hits
+  FROM articles, log
+  WHERE path LIKE '%' || slug || '%'
+  GROUP BY articles.title
+  ORDER BY hits DESC
   limit 3;
 
 -- 2.
-select name, count(*) as views
-  from (select name, slug from articles, authors where author = authors.id)as sq, log
-  where path like '%' || sq.slug || '%'
-  group by name
-  order by views desc;
+SELECT name, COUNT(name) AS views
+  FROM (SELECT name, slug FROM articles, authors WHERE author = authors.id) AS sq, log
+  WHERE path LIKE '%' || sq.slug || '%'
+  GROUP BY name
+  ORDER BY views DESC;
 
 -- 3.
-select count(dt) from (select date(time) as dt from log group by date(time)) as sq;
-select dt, status, count(status) from (select date(time) as dt, status from log) as sq group by dt, status;
+CREATE VIEW daily_status_log AS
+  SELECT dt, status, COUNT (status) AS status_count
+    FROM (SELECT DATA (time) AS dt, status FROM log) AS sq
+    GROUP BY dt, status;
 
-create view daily_status_log as
-  select dt, status, count(status) as hits
-    from (select date(time) as dt, status from log) as sq
-    group by dt, status;
-
-select dt, sum(case when status like '%404 NOT FOUND%' THEN hits END) / sum(hits) from daily_status_log group by dt;
-
-select dt, error_freq
-  from (select dt, sum(case when status like '%404 NOT FOUND%' THEN hits END) / sum(hits) as error_freq
-          from daily_status_log group by dt) as sq
-  where error_freq > 0.01;
+SELECT dt, error_freq
+  FROM (FROM dt, SUM(CASE WHEN status LIKE '%404 NOT FOUND%' THEN status_count END) / SUM(status_count) AS error_freq
+          FROM daily_status_log GROUP BY dt) AS sq
+  WHERE error_freq > 0.01;
